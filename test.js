86c84ff9aee44fe9863064d6a8f62b3309f7b953
sathurnesh <sathurneshraj@gmail.com>
2026-05-30 16:59:26 +0530

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+//var browserName="Chrome" //global variable
+
+//function getBrowserVersion()
+//{
+  //  if (browserName=="Chrome")
+ //{
+   // var browserName="Safari"  //local variable
+ //}
+
+ //}
+
+//getBrowserVersion()
+//console.log(browserName)
+
+let browserName="Chrome" //global variable
+
+function getBrowserVersion()
+{
+    if (browserName=="Chrome")
+ {
+    let browserName="Safari"  //local variable
+ }
+console.log(browserName)
+ }
+ 
+
+getBrowserVersion() 
